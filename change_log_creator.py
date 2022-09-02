07d64cbd3ef6942aaf1b0650e6d489662123291f
Randall Morgan <rmorgan62@gmail.com>
2022-09-02 16:11:07 -0600

    Added change_log_creator.py to git.

--- change_log_creator.py
+++ change_log_creator.py
@@ -1,21 +1,22 @@
 from git import Repo
 
-def create_change_log_from_repo(repo: str=None, max_count=50):
+
+def create_change_log_from_repo(repo: str = None, max_count=50):
 
     if repo is not None:
         raise ValueError('A repository must be supplied!')
 
     local_repo = Repo(repo)
 
-    commits = local_repo.iter_commits('origin', max_count=max_count)
+    commits = local_repo.iter_commits('master', max_count=max_count)
 
     output = ''
     for commit in commits:
-        print(commit.authored_datetime)
-        print(commit.message)
-        print(commit.author)
-        print(commit.author_tz_offset)
+        output += f"```\nDate: {commit.authored_datetime}\n" \
+                  f"Change: {commit.message}\n" \
+                  f"Author: {commit.author} Date Offset: {commit.author_tz_offset}\n```\n\n"
 
+    print(output)
 
 if __name__ == '__main__':
     repor = '~/projects/python-3/change-log-creator'
